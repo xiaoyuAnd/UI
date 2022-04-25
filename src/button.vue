e4eea195d210fb77d58128692ad4ecdde2805c6f
Frank <2521647197@qq.com>
2022-04-25 22:39:02 +0800

    实现iconPosition，并开启验证iconPosition必须是left或者right

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,32 @@
 <template>
-  <button class="r-button">按钮</button>
+  <button class="r-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+
+  </button>
 </template>
 
 <script>
+
 export default {
-  name: "r-button"
+  name: "r-button",
+  // props: ['icon', 'iconPosition']
+  props:{
+    icon:{},
+    iconPosition:{
+      type: String,
+      required: false,
+      default: 'left',
+      validator: function (value) {
+        // 这个值必须匹配下列字符串中的一个
+        return ['left', 'right', ].indexOf(value) !== -1
+      }
+    }
+  }
 }
 </script>
 
@@ -16,6 +38,10 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
 
   &:hover {
     border-color: var(--border-color-hover);
@@ -27,6 +53,27 @@ export default {
 
   &:focus {
     outline: none;
+  }
+
+  > .icon {
+    order: 1
+  }
+
+  > .content {
+    order: 2;
+    padding-left: .1em
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+    }
+
+    > .content {
+      order: 1;
+      padding-left: 0;
+      padding-right: .1em
+    }
   }
 }
 
