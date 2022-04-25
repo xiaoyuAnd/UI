a0d357ab627970d13d8758a84bc840a4dc9da5d1
Frank <2521647197@qq.com>
2022-04-25 21:26:55 +0800

    安装parcel，创建button文件，完成button文件基本样式，把vue版本换成2

---
+++ src/button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="r-button">按钮</button>
+</template>
+
+<script>
+export default {
+  name: "r-button"
+}
+</script>
+
+<style scoped lang="scss">
+.r-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+
+</style>
